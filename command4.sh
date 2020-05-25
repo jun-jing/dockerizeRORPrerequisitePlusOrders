6d6f06acff2fa7bacbfdf00790b5ee57ab431842
zjj <jessejunjing@163.com>
2020-05-26 07:37:30 +0800

    revise command4.sh, revise code.

--- command4.sh
+++ command4.sh
@@ -1,7 +1,7 @@
-chmod -R 777 randomName/
+chmod -R 777 randomName
 echo "check randomName is 777."
-(cd randomName/ && docker-compose build)
-(cd randomName/ && docker-compose run web yarn install)
-(cd randomName/ && docker-compose run web bin/rails webpacker:install)
-(cd randomName/ && docker-compose run web bin/rails webpacker:install:react)
-(cd randomName/ && docker-compose up -d)
+(cd randomName && docker-compose build)
+(cd randomName && docker-compose run web yarn install)
+(cd randomName && docker-compose run web bin/rails webpacker:install)
+(cd randomName && docker-compose run web bin/rails webpacker:install:react)
+(cd randomName && docker-compose up -d)

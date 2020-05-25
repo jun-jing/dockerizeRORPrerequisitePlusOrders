609ef1855730b592785cc942cd0ee6398bc3f81a
zjj <jessejunjing@163.com>
2020-05-26 07:18:29 +0800

    revise command4.sh, add echo mention.

--- command4.sh
+++ command4.sh
@@ -1,4 +1,5 @@
 chmod -R 777 randomName/
+echo "check randomName is 777."
 (cd randomName/ && docker-compose build)
 (cd randomName/ && docker-compose run web yarn install)
 (cd randomName/ && docker-compose run web bin/rails webpacker:install)

ccd9e4210c97ba4367238c7dd5b9de49e9a10d42
zjj <jessejunjing@163.com>
2020-05-26 07:11:10 +0800

    revise command4.sh, add chmod randomName.

--- command4.sh
+++ command4.sh
@@ -1,3 +1,4 @@
+chmod 777 -R randomName/
 (cd randomName/ && docker-compose build)
 (cd randomName/ && docker-compose run web yarn install)
 (cd randomName/ && docker-compose run web bin/rails webpacker:install)

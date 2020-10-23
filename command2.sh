1aefb6f9d8d426c52293f22bc8544ba7e749151f
zjj <jessejunjing@163.com>
2020-10-24 00:42:34 +0800

    command add mirror change scripts.

--- command2.sh
+++ command2.sh
@@ -1,4 +1,4 @@
-# gem sources --remove https://rubygems.org/
-# gem sources -a https://gems.ruby-china.com
+gem sources --remove https://rubygems.org/
+gem sources -a https://gems.ruby-china.com
 gem install rails 
 rails new bootTemplateWithROR-vintageMarket --skip-test --skip-bundle

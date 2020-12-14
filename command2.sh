084c4f51f5a3fd2f3509047c393602596fa62dfa
zjj <jessejunjing@163.com>
2020-12-14 14:34:13 +0800

    uncomment sources, comment无法修复bug。

--- command2.sh
+++ command2.sh
@@ -1,5 +1,5 @@
-# gem sources --remove https://rubygems.org/
-# gem sources -a https://gems.ruby-china.com
+gem sources --remove https://rubygems.org/
+gem sources -a https://gems.ruby-china.com
 # gem install bundler
 gem install rails 
 rails new auctionGift --skip-test --skip-bundle

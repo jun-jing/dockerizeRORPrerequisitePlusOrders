8f7e3c3cc360a7468ac6ae83d7cce2060b02f7d9
zjj <jessejunjing@163.com>
2020-12-14 13:56:29 +0800

    添加gem install bundler

--- command2.sh
+++ command2.sh
@@ -1,5 +1,6 @@
 gem sources --remove https://rubygems.org/
 gem sources -a https://gems.ruby-china.com
+gem install bundler
 gem install rails 
 rails new auctionGift --skip-test --skip-bundle
 # rails new auctionGift

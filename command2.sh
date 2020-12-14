b11476470234d5ab057336d255bbf0b0a83e9a68
zjj <jessejunjing@163.com>
2020-12-14 15:04:15 +0800

    设置指定rails版本看是否能够修复spec报错。

--- command2.sh
+++ command2.sh
@@ -1,5 +1,6 @@
 gem sources --remove https://rubygems.org/
 gem sources -a https://gems.ruby-china.com
 # gem install bundler
-gem install rails 
+gem install rails  -v 6.0.2.3
+# gem install rails
 rails new auctionGift --skip-test --skip-bundle

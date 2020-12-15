7d73134c03236a35e2b90b6fd8344d4bb3cb4b9b
zjj <jessejunjing@163.com>
2020-12-15 13:36:34 +0800

    can not use version number when creating new app...

--- command2.sh
+++ command2.sh
@@ -2,7 +2,4 @@ gem sources --remove https://rubygems.org/
 gem sources -a https://gems.ruby-china.com
 # 指定特定版本, 尝试解决spec nomethoderror问题
 gem install rails -v 6.0.3.4
-
-
-
-rails 6.0.3.4 new auctionGift --skip-test --skip-bundle
+rails new auctionGift --skip-test --skip-bundle

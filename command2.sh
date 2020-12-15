e15096a08c568d3176a5306dfe5d4c3efb33a8d9
zjj <jessejunjing@163.com>
2020-12-15 12:00:03 +0800

    recover last change.

--- command2.sh
+++ command2.sh
@@ -1,10 +1,5 @@
 gem sources --remove https://rubygems.org/
 gem sources -a https://gems.ruby-china.com
-# fixed spec nomethod issue
-# /usr/src/app/auctionGift/bin/spring:5:in `<top (required)>': 
-#   undefined method `specs' for nil:NilClass (NoMethodError)
-# https://stackoverflow.com/questions/59410402/webpacker-error-while-creating-ruby-on-rails-app
-gem install yarn
 # gem install bundler
 gem install rails
 

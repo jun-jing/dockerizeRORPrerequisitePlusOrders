ed5f41c70400ee562c5a4373ad274a2cfcb4e5b9
zjj <jessejunjing@163.com>
2020-12-15 13:27:42 +0800

    deal with spec

--- command2.sh
+++ command2.sh
@@ -1,8 +1,8 @@
 gem sources --remove https://rubygems.org/
 gem sources -a https://gems.ruby-china.com
-# gem install bundler
-gem install rails
+# 指定特定版本, 尝试解决spec nomethoderror问题
+gem install rails -v 6.0.3.4
 
 
 
-rails new auctionGift --skip-test --skip-bundle
+rails 6.0.3.4 new auctionGift --skip-test --skip-bundle

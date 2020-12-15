ccb481c7e7ac40d3fb2186705916401d09b49eca
zjj <jessejunjing@163.com>
2020-12-15 11:09:42 +0800

    取消跳过webpack

--- command2.sh
+++ command2.sh
@@ -2,4 +2,4 @@ gem sources --remove https://rubygems.org/
 gem sources -a https://gems.ruby-china.com
 # gem install bundler
 gem install rails
-rails new auctionGift --skip-test --skip-bundle --skip-webpack-install
+rails new auctionGift --skip-test --skip-bundle
